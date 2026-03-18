b231e7bc420bf52816b4debe43e7d9373a2ee2ee
alailayangan27-png <alailayangan27@gmail.com>
2026-03-19 01:30:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
+let currentWallet = "";
+
 function generate() {
 
   const name = document.getElementById("name").value || "No Name";
   const username = document.getElementById("username").value || "@unknown";
   const bio = document.getElementById("bio").value || "-";
   const skills = document.getElementById("skills").value || "-";
+  const wallet = document.getElementById("wallet").value || "-";
+
+  currentWallet = wallet;
 
   document.getElementById("cName").innerText = name;
   document.getElementById("cUser").innerText = username;
   document.getElementById("cBio").innerText = bio;
   document.getElementById("cSkills").innerText = "Skills: " + skills;
+  document.getElementById("cWallet").innerText = wallet;
 
   const avatar = document.getElementById("avatar");
   avatar.innerText = name.charAt(0).toUpperCase();
 
   document.getElementById("card").classList.remove("hidden");
 
-  const data = `${name} | ${username} | ${bio} | ${skills}`;
+  // QR SOLANA FORMAT
+  const qrData = wallet !== "-" ? `solana:${wallet}` : "No Wallet";
 
-  QRCode.toCanvas(document.getElementById("qr"), data);
+  QRCode.toCanvas(document.getElementById("qr"), qrData, function (error) {
+    if (error) console.error(error);
+  });
 }
 
-function download() {
+// Copy wallet
+function copyWallet() {
+  if (!currentWallet) return alert("Wallet kosong!");
+  navigator.clipboard.writeText(currentWallet);
+  alert("Wallet copied!");
+}
 
+// Download card
+function download() {
   const card = document.getElementById("card");
 
   html2canvas(card).then(canvas => {
     const link = document.createElement("a");
-    link.download = "lobstar-id.png";
+    link.download = "lobstar-wallet.png";
     link.href = canvas.toDataURL();
     link.click();
   });
-
 }
