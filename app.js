9386cbf2d145a74e110a8f1867c2065670c74e97
alailayangan27-png <alailayangan27@gmail.com>
2026-03-19 01:21:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ function generate() {
   document.getElementById("cBio").innerText = bio;
   document.getElementById("cSkills").innerText = "Skills: " + skills;
 
-  // Avatar dari huruf pertama
   const avatar = document.getElementById("avatar");
   avatar.innerText = name.charAt(0).toUpperCase();
 
   document.getElementById("card").classList.remove("hidden");
 
-  // QR Code
   const data = `${name} | ${username} | ${bio} | ${skills}`;
 
-  QRCode.toCanvas(document.getElementById("qr"), data, function (error) {
-    if (error) console.error(error);
-  });
+  QRCode.toCanvas(document.getElementById("qr"), data);
 }
 
 function download() {
@@ -30,7 +26,7 @@ function download() {
 
   html2canvas(card).then(canvas => {
     const link = document.createElement("a");
-    link.download = "digital-id.png";
+    link.download = "lobstar-id.png";
     link.href = canvas.toDataURL();
     link.click();
   });
